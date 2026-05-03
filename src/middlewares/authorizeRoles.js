28ded6c4e8386c1f4fa5dcb149839f1e1e963c23
Roberto <rivelinorsousa92@gmail.com>
2026-05-03 00:33:17 -0300

    feat: add authorize roles middleware

---
+++ src/middlewares/authorizeRoles.js
@@ -0,0 +1,13 @@
+export default function authorizeRoles(...allowedRoles) {
+  return (req, res, next) => {
+    const userRole = req.user?.role;
+
+    if (!allowedRoles.includes(userRole)) {
+      return res.status(403).json({
+        message: "Você não tem permissão para acessar este recurso",
+      });
+    }
+
+    next();
+  };
+}
